9ceb11d36e12227af138eb4002faab2f139a39e9
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-27 23:07:20 +0530

    upload multiple files

--- index.js
+++ index.js
@@ -7,24 +7,60 @@ app.set('view engine', 'ejs');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'files/');
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        cb(null, 'abhishek-ki-file' + Date.now() + '-' + file.originalname);
+        cb(null, Date.now() + '-' + file.originalname);
     }
 })
 
 const upload = multer({ storage });
 
-app.get('/', (req, res) => {
-    return res.render('Form');
+app.get('/upload-multi-form', (req, res) => {
+    return res.render('multi-form');
 });
 
+// app.post('/upload-file-multi', upload.fields([
+//     { name: 'profile', maxCount: 4 },
+//     { name: 'resume', maxCount: 1 },
+//     { name: 'report', maxCount: 1 }
+// ]),
+//     (req, res) => {
+//         console.log('save');
+//         return res.send("File uploaded successfully!");
+//     });
+
+// app.post('/upload-file-multi', upload.fields([{ name: 'profile', maxCount: 4 }, { name: 'resume', maxCount: 1 }, { name: 'report', maxCount: 1 }]),
+//     (req, res) => { console.log('save'); return res.send("File uploaded successfully!"); });
+
+
+app.post('/upload-file-multi',
+    upload.fields(
+        [
+            { name: 'profile', maxCount: 4 },
+            { name: 'resume', maxCount: 1 },
+            { name: 'report', maxCount: 1 }
+        ]
+    ),
+    (req, res) => {
+        console.log('save');
+        return res.send("File uploaded successfully!");
+    }
+);
+
+
+app.get('/', (req, res) => {
+    return res.status(200).redirect('/single-file');
+})
+app.get('/single-file', (req, res) => {
+    return res.render('Form');
+});
 
 app.post('/upload-file', upload.single("profile"), (req, res) => {
     console.log(req.file);
     res.send("File uploaded successfully!");
 });
+
 
 app.listen(8080, (err) => {
     if (!err) {
