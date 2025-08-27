933ae8ac421b3736c102bda8c14a2558d8e1054d
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-27 21:06:20 +0530

    handle Post

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express()
 
-app.set('view engin', 'ejs');
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    return res.render('Form');
+});
+
+app.post('/upload-file', (req, res) => {
+    console.log(req);
+});
 
 app.listen(8080, (err) => {
     if (!err) {
